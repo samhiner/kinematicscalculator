581501c6ed84a53ec41f3b2d633f29411e61265c
smhnr27 <25464398+smhnr27@users.noreply.github.com>
2018-07-14 22:15:13 -0400

    Basic Graph Updating on Drag
    Can be misleading at times as the changes are measured over such small increments, but it fully works.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# code
-small projects not worthy of their own repo
+### Kinematics Calculator
+
+See how graphs of position, velocity, and acceleration against time are related to each other by seeing how modifying one changes the values of the other graphs. 
+
+### Features
+
+* Drag points on any one of the graphs to see how it affects the other graphs.
+* Change the number of seconds you see, and how many changeable points there are per second.
+* Support for fractions of a second and one point for multiple seconds.
+
+## Upcoming
+
+* Insert your own function and see how the three graphs look according that function.
+* See what a ball moving according to those graphs would look like.
+* Create embedding code for the graphs.
+* Templates for graphs which pertain to usual kinematics problems.
--- javascript/kinematicscalculator/calculatorlogic.js
+++ javascript/kinematicscalculator/calculatorlogic.js
@@ -1,20 +1,20 @@
 class graph extends Chart {
-	constructor(id,type) {
+	//make a line or bar chart using a given canvas
+	constructor(id,sizeData) {
 		var canvas = document.getElementById(id).getContext('2d');
-		if (type == 'bar') {
-			var labels = ['0-1', '1-2', '2-3', '3-4', '4-5']
+		if (id == 'acceleration') {
+			var firstPoint = null;
 		} else {
-			var labels = ['0', '1', '2', '3', '4', '5']
+			var firstPoint = 'black';
 		}
-
 		super(canvas, {
-			type: type,
+			type: 'line',
 			data: {
-				labels: labels,
+				labels: sizeData[0],
 				datasets: [
 					{
-						data: [100, 19, 29, 20, 30, 20],
-						fill: false
+						data: sizeData[1],
+						fill: false,
 					}
 				]
 			},
@@ -23,36 +23,94 @@ class graph extends Chart {
 					display: false
 				},
 				dragData: true,
-				//add it so on every movement alignGraphs is triggered using the functions in the docs for dragData
-				barPercentage: 1.0,
+				onDrag: function (event, datasetIndex, index, value) {
+					alignGraphs(id);
+				},
 				scales: {
 					xAxes: [{
-						barPercentage: 1,
-						categoryPercentage: 1
-					}],
+						ticks: {
+							autoSkip: true,
+							maxTicksLimit: 5
+						}
+					}]
 				}
 			}
 		});
 	}
 
-	changeVals(array) {
+	//get an array of the y values of the chart
+	getData() {
+		return this.config.data.datasets[0].data
+	}
+
+	changeData(array) {
 		this.config.data.datasets[0].data = array;
 		this.update();
 	}
 
 }
 
+
 function alignGraphs(origin) {
-	arr = ['position','velocity','acceleration'];
-	arr.indexOf(origin)
-	//make it so for every -1 you do another multiplication by time?
-	//will get nums for each second and output an array of them to changeVals
-	//have each second be based on last one 
-	//x-not in equation would be at start of that second if you are trying to find end of second
+
+	x = position.getData()
+	v = velocity.getData()
+	a = acceleration.getData()
+
+	var timesT = (oldLevel, newLevel, t) => newLevel[t-1] + (oldLevel[t] + oldLevel[t-1])/2;
+	var divT = (oldLevel, newLevel, t) => (oldLevel[t] - oldLevel[t-1]) * 2;
+
+	//ISSUE make it so the proper times/div is triggered without explicit ifs so I can add this to the main class bc thatd be cool
+	if (origin == 'position') {
+		for (var t = 1; t < 6; t++) {
+			v[t] = divT(x, v, t);
+			a[t] = divT(v, a, t);
+		}
+	} else if (origin == 'velocity') {
+		for (var t = 1; t < 6; t++) {
+			x[t] = timesT(v, x, t);
+			a[t] = divT(v, a, t);
+		}
+	} else if (origin == 'acceleration') {
+		for (var t = 1; t < 6; t++) {
+			v[t] = timesT(a, v, t);
+			x[t] = timesT(v, x, t);
+		}
+	}
+
+	position.changeData(x);
+	velocity.changeData(v);
+	acceleration.changeData(a);
+
+}
+/*DOCS (ISSUE: make this accurate):
+
+//position = position last second + whatever the effective velocity was over the last second
+//effective velocty is average of last second's and this second's velocity. returns same value as v0t + 1/2at^2.
+x[t] = x[t - 1] + ((v[t] + v[t-1])/2)
+
+
+//velocity = difference between current position and position one second ago times 2
+//times 2 because otherwise you get Vavg for the second between the instant being measured and the last second
+//this gets V at the end of that second (now)
+v[t] = (x[t] - x[t-1]) * 2
+
+*/
+
+function makeGraphData(size) {
+	labels = [0];
+	vals = [0]
+	for (var x = 1; x < size; x++) {
+		labels[x] = labels[x - 1] + 1;
+		vals[x] = Math.random() * 100;
+	}
+	return [labels, vals];
 }
 
-var position = new graph('position','line');
-var velocity = new graph('velocity','line');
-var acceleration = new graph('acceleration','bar');
+aSize = 6;
 
-//acceleration.changeVals([100, 100, 100, 100, 100, 100])
+var position = new graph('position',makeGraphData(aSize));
+var velocity = new graph('velocity',makeGraphData(aSize));
+var acceleration = new graph('acceleration',makeGraphData(aSize));
+
+alignGraphs('acceleration')
