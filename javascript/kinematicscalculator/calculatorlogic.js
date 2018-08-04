5727f1cf6da613374a475d48ff9ee8e873c39607
samhiner <25464398+samhiner@users.noreply.github.com>
2018-08-04 16:46:52 -0400

    Make acceleration more accurate (constant velocities) and refactor general logic into session class.

--- javascript/kinematicscalculator/calculatorlogic.js
+++ javascript/kinematicscalculator/calculatorlogic.js
@@ -3,9 +3,9 @@ class graph extends Chart {
 	constructor(id,sizeData,seconds) {
 		var canvas = document.getElementById(id).getContext('2d');
 		if (id == 'acceleration') {
-			var firstPoint = null;
+			var lineTension = 0;
 		} else {
-			var firstPoint = 'black';
+			var lineTension = 0.4;
 		}
 		super(canvas, {
 			type: 'line',
@@ -19,12 +19,17 @@ class graph extends Chart {
 				]
 			},
 			options: {
+				elements: {
+					line: {
+						tension: lineTension 
+					}
+				},
 				legend: {
 					display: false
 				},
 				dragData: true,
 				onDrag: function (event, datasetIndex, index, value) {
-					alignGraphs(id);
+					userSess.alignGraphs(id);
 				},
 				scales: {
 					xAxes: [{
@@ -40,7 +45,7 @@ class graph extends Chart {
 
 	//get an array of the y values of the chart
 	getData() {
-		return this.config.data.datasets[0].data
+		return this.config.data.datasets[0].data;
 	}
 
 	changeData(array) {
@@ -50,40 +55,100 @@ class graph extends Chart {
 
 }
 
+class sess {
+	//create new graphs with random data and make variables and lambdas which describe the graphs
+	constructor() {
+		const totalSeconds = this.getGraphSize();
 
-function alignGraphs(origin, seconds) {
+		this.timesT = (oldLevel, newLevel, t) => newLevel[t-1] + (oldLevel[t] + oldLevel[t-1])/2;
+		this.divT = (oldLevel, newLevel, t) => (oldLevel[t] - oldLevel[t-1]) * 2;
 
-	x = position.getData()
-	v = velocity.getData()
-	a = acceleration.getData()
+		this.position = new graph('position',this.initGraph(),totalSeconds);
+		this.velocity = new graph('velocity',this.initGraph(),totalSeconds);
+		this.acceleration = new graph('acceleration',this.initGraph(),totalSeconds);
 
-	var timesT = (oldLevel, newLevel, t) => newLevel[t-1] + (oldLevel[t] + oldLevel[t-1])/2;
-	var divT = (oldLevel, newLevel, t) => (oldLevel[t] - oldLevel[t-1]) * 2;
-
-	//ISSUE make it so the proper times/div is triggered without explicit ifs so I can add this to the main class bc thatd be cool
-	if (origin == 'position') {
-		for (var t = 1; t < numPoints; t += 1/resolution) {
-			v[t] = divT(x, v, t);
-			a[t] = divT(v, a, t);
-		}
-	} else if (origin == 'velocity') {
-		for (var t = 1; t < numPoints; t += 1/resolution) {
-			x[t] = timesT(v, x, t);
-			a[t] = divT(v, a, t);
-		}
-	} else if (origin == 'acceleration') {
-		for (var t = 1; t < numPoints; t += 1/resolution) {
-			v[t] = timesT(a, v, t);
-			x[t] = timesT(v, x, t);
-		}
+		this.alignGraphs('acceleration');
 	}
 
-	position.changeData(x);
-	velocity.changeData(v);
-	acceleration.changeData(a);
+	//grab the graph measurements from the user inputs and clean them into useable data which is used in many of this classes methods
+	getGraphSize() {
+		const totalSeconds = Number(document.getElementById('seconds').value) + 1;
+
+		this.resolution = Number(document.getElementById('resolution').value);
+		//substract (resolution - 1) does not include any points after the max number 
+		//ex: if there are five points and 4 resolution, 3 points are not generated after 5.0
+		this.numPoints = totalSeconds * this.resolution - (this.resolution - 1);
+
+		console.log(totalSeconds + ' ' + this.resolution);
+
+		return totalSeconds;
+	}
+
+	//make labels and random placeholder data based on number of seconds and how many points there should be per second (resolution)
+	initGraph() {
+		let labels = [0];
+		let vals = [0]
+		//subtract (res - 1) bc otherwise if the max is 5 and res is 4 the max will be 5.75 as it made 3 extra decimals for the max.
+		for (var x = 1; x < this.numPoints; x++) {
+			let newLabel = labels[x - 1] + 1/this.resolution;
+			//if the number on the x axis is very close to a whole number, round it to that number. prevents 1 being 0.9999 when resolution is 3.
+			if ((String(newLabel).indexOf('.') == -1) || (String(newLabel).split('.')[1].indexOf('9999') != -1) || (String(newLabel).split('.')[1].indexOf('0000') != -1)) {
+				newLabel = Math.round(newLabel);
+			}
+			labels[x] = newLabel;
+			vals[x] = Math.random() * 100 - 50;
+		}
+
+		return [labels, vals];
+	}
+
+	//change the graphs so their data is based on the data of the origin graph
+	alignGraphs(origin) {
+
+		let x = this.position.getData();
+		let v = this.velocity.getData();
+		let a = this.acceleration.getData();
+
+		//TODO make it so the for loop isn't repeated three times.
+		if (origin == 'position') {
+			for (var t = 1; t < this.numPoints; t++) {
+				v[t] = this.divT(x, v, t);
+				a[t] = this.divT(v, a, t);
+			}
+		} else if (origin == 'velocity') {
+			for (var t = 1; t < this.numPoints; t++) {
+				x[t] = this.timesT(v, x, t);
+				a[t] = this.divT(v, a, t);
+			}
+		} else if (origin == 'acceleration') {
+			for (var t = 1; t < this.numPoints; t++) {
+				v[t] = this.timesT(a, v, t);
+				x[t] = this.timesT(v, x, t);
+			}
+		}
+
+		this.position.changeData(x);
+		this.velocity.changeData(v);
+		this.acceleration.changeData(a);
+
+	}
+
+	//change the number of points on the screen while preserving as much of the data from before the resizing as possible
+	//TODO: this
+	graphPointsChange() {
+		getGraphSize()
+
+		let vals = this.acceleration.getData();
+
+		return;
+		//IDEA: maybe change the x.y.z.labels property of old canvases instead of making new ones at the end
+	}
 
 }
-/*DOCS (ISSUE: make this accurate):
+
+var userSess = new sess();
+
+/*DOCS (ISSUE: make this more accurate):
 
 //position = position last second + whatever the effective velocity was over the last second
 //effective velocty is average of last second's and this second's velocity. returns same value as v0t + 1/2at^2.
@@ -96,40 +161,3 @@ x[t] = x[t - 1] + ((v[t] + v[t-1])/2)
 v[t] = (x[t] - x[t-1]) * 2
 
 */
-
-//make labels and random placeholder data based on number of seconds and how many points there should be per second (resolution)
-function makeGraphData(seconds, resolution) {
-	labels = [0];
-	vals = [0]
-	//subtract res - 1 bc otherwise if the max is 5 and res is 4 the max will be 5.75 as it made 3 extra decimals for the max.
-	for (var x = 1; x < (seconds * resolution) - (resolution - 1); x++) {
-		newLabel = labels[x - 1] + 1/resolution;
-		//if the number on the x axis is very close to a whole number, round it to that number. prevents 1 being 0.9999 when resolution is 3.
-		if ((String(newLabel).indexOf('.') == -1) || (String(newLabel).split('.')[1].indexOf('9999') != -1) || (String(newLabel).split('.')[1].indexOf('0000') != -1)) {
-			newLabel = Math.round(newLabel)
-		}
-		labels[x] = newLabel
-		vals[x] = Math.random() * 100 - 50;
-	}
-	return [labels, vals];
-}
-
-function resetGraph() {
-	return;
-}
-
-//MAJOR ISSUE: this should run in resetGrapoh but that should be in a class so p, v, and a are accesable by other functions
-//IDEA: once in resetGraph maybe find a way to not randomize data after the first time so you can get higher res without starting over
-//ISSUE: some number/resolution combination do not go 1-2-3-4 but do random numbers on x-axis.
-var totalSeconds = Number(document.getElementById('seconds').value) + 1;
-var resolution = Number(document.getElementById('resolution').value);
-console.log(totalSeconds + ' ' + resolution)
-var numPoints = totalSeconds * resolution - (resolution - 1);
-
-var position = new graph('position',makeGraphData(totalSeconds, resolution),totalSeconds);
-var velocity = new graph('velocity',makeGraphData(totalSeconds, resolution),totalSeconds);
-var acceleration = new graph('acceleration',makeGraphData(totalSeconds, resolution),totalSeconds);
-
-alignGraphs('acceleration', seconds)
-
-//ISSUE: maybe put this in a class idk
